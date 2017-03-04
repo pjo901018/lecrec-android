3991eb02e298beaa7c4312a1e0fabdf6d99f0a38
pjo901018 <pjo901018@korea.ac.kr>
2017-03-04 11:24:31 +0900

    Code Review

--- app/src/main/java/com/lecrec/lecrec/consts/URL.java
+++ app/src/main/java/com/lecrec/lecrec/consts/URL.java
@@ -4,9 +4,11 @@ import com.lecrec.lecrec.BuildConfig;
 
 public class URL {
     public static final String GET_API_URL() {
-        final String url = BuildConfig.DEBUG ? "http://211.249.62.164/api/" : "http://211.249.62.164/api/";
+        //final String url = BuildConfig.DEBUG ? "http://211.249.62.164/api/" : "http://211.249.62.164/api/";
+        final String url = BuildConfig.DEBUG ? "http://yangyinetwork.asuscomm.com:88/api/" : "http://yangyinetwork.asuscomm.com:88/api/";
         return url;
     }
 
-    public static final String SITE_URL = "http://211.249.62.164/";
+    //public static final String SITE_URL = "http://211.249.62.164/";
+    public static final String SITE_URL = "http://yangyinetwork.asuscomm.com:88/";
 }
